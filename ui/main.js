08a0fca5b0662ee5366825dd3969e7b125837578
Tejasviohlan <30663549+Tejasviohlan@users.noreply.github.com>
2018-02-22 00:44:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var element=document.getElementById('main-text');
 element;
 element.innerHTML='tejasvi ohlan';
 
-var image=document.getelementbyid('madi');
+var image=document.getElementById('madi');
 img.onclick= function(){
     img.style.marginleft='100px';
 }
